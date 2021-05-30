5178babe3bb802c5b5d754d1d502673c787825a9
xogud0930 <xogud0930@gmail.com>
2021-05-31 01:15:54 +0900

    5.30 commit socket 구현

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 6050;
+const bodyParser = require('body-parser')
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+const server = require('http').createServer(app).listen(port, () => {
+    console.log("Express server listening on port " + port)
+})
+
+app.use('/public', express.static('./public'))
+app.get('/', (req, res) => {
+    res.redirect(302, '/public')
+})
+
+const socketio = require('socket.io')
+const io = socketio(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('사용자 접속: ', socket.client.id)
+
+    socket.on('chat-msg', (msg) => {
+        console.log(`[${msg.time}]${msg.name} : ${msg.message}`)
+
+        io.emit('chat-upload', msg)
+    })
+})
