bee00085c15223b6d035b25e05c766536b068d4a
xogud0930 <xogud0930@gmail.com>
2021-06-02 01:24:58 +0900

    6.1 home 입장 / 퇴장 수정 socket 중복 렌더 오류 해결 lobby 플레이어 리스트 인게임 표시

--- app.js
+++ app.js
@@ -24,22 +24,69 @@ const io = socketio(server, {
     }
 });
 
+var playerList = [];
+var roomPlayerList = [];
+
+const findItem = (arr, item, type = false) => {
+    var isState = arr !== '' &&
+        arr.find((list, idx) => {
+            if(type === 'id' & list.id === item.id) {
+                arr.splice(idx, 1)
+                return true;
+            }
+            else if(list.id === item.id) {
+                if(type === 'on' || type === 'off' ) {
+                    arr[idx].room = item.room
+                    arr[idx].state = type
+                }
+                else if(type === 'sub') arr.splice(idx, 1)
+                return true;
+            }
+        });
+    return !isState;
+}
+
 io.on('connection', (socket) => {
     console.log(`${socket.client.id} connected`)
+    
+    socket.on('lobby-join', (msg) => {
+        console.log(`${msg.name} ${msg.room}-join`)
+
+        msg = {
+            type: findItem(playerList, msg) ? 'JOIN' : 'NONE',
+            name: msg.name,
+            message: `${msg.name}님이 Lobby에 입장하였습니다.`,
+            room: msg.room,
+            id: socket.client.id,
+        }
+
+        socket.join(msg.room);
+        io.to(msg.room).emit('chat-join', msg)
+    })
 
     socket.on('room-join', (msg) => {
         console.log(`${msg.name} ${msg.room}-join`)
 
         msg = {
-            type: 'SYSTEM',
+            type: 'ROOM-JOIN',
             name: msg.name,
-            message: `${msg.name} 님이 접속하였습니다.`,
+            message: `${msg.name}님이 Room ` + msg.room
+            + `에 입장하였습니다.`,
             room: msg.room,
             id: socket.client.id,
         }
+        if(findItem(playerList, {...msg, id: socket.client.id})) {
+            playerList.push({name: msg.name, room: msg.room, id: socket.client.id, state: 'off'})    
+        // }
+        // if(findItem(roomPlayerList, msg)) {
+        //     findItem(playerList, msg, 'off')
+        //     roomPlayerList.push({name: msg.name, room: msg.room, id: socket.client.id, state: 'on'})    
         
-        socket.join(msg.room);
-        io.to(msg.room).emit('chat-join', msg)
+            socket.join(msg.room);
+            io.to('lobby').emit('chat-upload', msg)
+            io.to('lobby').emit('chat-list', playerList)
+            io.to(msg.room).emit('chat-upload', msg)
+        }
     })
 
     socket.on('chat-msg', (msg) => {
@@ -50,27 +97,81 @@ io.on('connection', (socket) => {
     })
 
     socket.on('chat-list', (data) => {
+        if(findItem(playerList, {...data, id: socket.client.id})) {
+            playerList.push({name: data.name, room: data.room, id: socket.client.id, state: 'on'})    
+        }
+
         socket.join(data.room);
-        io.to(data.room).emit('chat-list', data.list)
+        io.to(data.room).emit('chat-list', playerList)
     })
 
-    socket.on('chat-leave', (msg) => {
+    socket.on('lobby-leave', (msg) => {
         console.log(`${msg.name} ${msg.room}-leave`)
 
         msg = {
-            type: 'SYSTEM',
+            type: 'LEAVE',
             name: msg.name,
-            message: `${msg.name} 님이 퇴장하였습니다.`,
+            message: `${msg.name}님이 퇴장하였습니다.`,
             room: msg.room,
             id: socket.client.id,
         }
 
         socket.join(msg.room);
         io.to(msg.room).emit('chat-upload', msg)
+
+        if(!findItem(playerList, msg, 'sub')) {
+            io.to(msg.room).emit('chat-list', playerList)
+        }
+    })
+
+    socket.on('room-leave', (msg) => {
+        console.log(`${msg.name} ${msg.room}-leave`)
+
+        // msg = {
+        //     type: 'ROOM-LEAVE',
+        //     name: msg.name,
+        //     message: `${msg.name}님이 퇴장하였습니다.`,
+        //     room: msg.room,
+        //     id: socket.client.id,
+        // }
+
+        findItem(playerList, msg, 'on')
+        findItem(roomPlayerList, msg, 'sub')
+
+        socket.join(msg.room);
+        io.to('lobby').emit('chat-list', playerList)
+        // io.to(msg.room).emit('chat-upload', msg)
     })
 
     socket.on('disconnect', () => {
-        console.log(`${socket.client.id} disconnected`)
+        var msg = {
+            type: '',
+            name: '',
+            message: '',
+            room: '',
+            id: '',
+        };
+        playerList.find((list, idx) => {
+            if(list.id === socket.client.id) {
+                msg = {
+                    type: 'LEAVE',
+                    name: list.name,
+                    message: `${list.name}님이 퇴장하였습니다.`,
+                    room: list.room,
+                    id: socket.client.id,
+                }
+                return true
+            }
+        })
 
+        findItem(playerList, {id : socket.client.id}, 'id')
+        findItem(roomPlayerList, {id : socket.client.id}, 'id')
+
+        socket.join('lobby');
+        io.to('lobby').emit('chat-list', playerList)
+        io.to('lobby').emit('chat-upload', msg)
+        io.to(msg.room).emit('chat-upload', msg)
+
+        console.log(`${socket.client.id} disconnected`)
     })
 })
